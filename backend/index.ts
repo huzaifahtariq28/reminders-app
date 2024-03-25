9c44b0fa905439da7f72f262fd14bfd012d5dcca
Huzaifah Tariq <huzaifahtariq028@gmail.com>
2024-03-25 07:39:16 +0500

    basic crud apis

--- backend/index.ts
+++ backend/index.ts
@@ -1,2 +1,13 @@
-let x = 1;
-console.log(x);
+import express from 'express';
+import remindersRouter from './routers/reminders';
+
+const app = express();
+
+app.use(express.json());
+app.use('/reminders', remindersRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8000, () => console.log('Server listening on port 8000...'));
